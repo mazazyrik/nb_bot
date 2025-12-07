f4ba38da7f8d5657c54ccbf7a67c705d4f8f525d
mazazyrik <117200062+mazazyrik@users.noreply.github.com>
2025-12-07 21:33:20 +0300

    fix: update Docker configuration to correct guest JSON file path and add SYSTEM_PROMPT.md for improved functionality

--- src/crud/models.py
+++ src/crud/models.py
@@ -7,13 +7,13 @@ from crud.enums import InstitutueEnum, RoleEnum
 class Admin(Model):
     id = fields.IntField(pk=True)
     username = fields.CharField(unique=True, max_length=100, null=True)
-    telegram_id = fields.IntField(unique=True, null=False)
+    telegram_id = fields.BigIntField(unique=True, null=False)
     role = fields.CharEnumField(enum_type=RoleEnum, null=False)
 
 
 class Visitor(Model):
     id = fields.IntField(pk=True)
-    telegram_id = fields.IntField(unique=True, null=False)
+    telegram_id = fields.BigIntField(unique=True, null=False)
     full_name = fields.CharField(max_length=100, null=False)
     program = fields.CharEnumField(enum_type=InstitutueEnum, null=True)
     grade = fields.CharField(max_length=100, null=False)
